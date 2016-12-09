b0eacf91f88dc475fb5ae8faf0e6275eeb6a97f0
Vien Tang <vientang@gmail.com>
2016-12-08 23:34:15 -0800

    Add background image, transition effects, and other styles

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 window.addEventListener('keydown', function(event) {
 	const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
 	const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-	console.log(key);
+	// handle errors
 	if (!audio) return;
-	audio.currentTime = 0; // rewind to the start
+	// configure audio 
+		// to rewind to the start on every key press
+	audio.currentTime = 0; 
 	audio.play();
+		// add a .playing class on key press
+	key.classList.add("playing");	
 });
+
+const keys = document.querySelectorAll(".key");
